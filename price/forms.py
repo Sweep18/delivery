377245b3d6b540d3dc5b4eab596335f44f6d87ad
Sweep18 <sweeps@mail.ru>
2016-12-07 14:38:22 +0300

    second

--- price/forms.py
+++ price/forms.py
@@ -2,13 +2,14 @@ from django import forms
 
 CHOICES_PRODUCT = [('potato', "Картошка"), ("cabbage", "Капуста")]
 CHOICES_CITY = [('Москва', "Москва"), ("Санкт-Петербург", "Санкт-Петербург")]
+WEIGHTS = [(1500, "1,5 т"), (3000, "3 т"), (5000, "5 т"), (10000, "10 т"), (20000, "20 т")]
 
 
 class PriceForm(forms.Form):
     product = forms.ChoiceField(label='Товар', choices=CHOICES_PRODUCT, widget=forms.Select)
     cityfrom = forms.ChoiceField(label='Откуда:', choices=CHOICES_CITY, widget=forms.Select)
     cityto = forms.ChoiceField(label='Куда:', choices=CHOICES_CITY, widget=forms.Select)
-    weight = forms.IntegerField(label='Вес(тонны):', initial=20000)
+    weight = forms.ChoiceField(label='Вес(тонны):', choices=WEIGHTS, widget=forms.Select, initial=20000)
     nds = forms.BooleanField(label='С НДС?', required=False)
     with_nds = forms.BooleanField(label='Без НДС?', required=False)
     nal = forms.BooleanField(label='Оплата наличными?', required=False)
--- price/views.py
+++ price/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import PriceForm
 import requests
+from geopy.distance import vincenty
+from geopy.geocoders import Yandex
 
 
 def price(request):
@@ -9,11 +11,10 @@ def price(request):
         if form.is_valid():
             url_price = 'https://groozgo.ru/api/order/calc_price'
             url_drive = 'https://groozgo.ru/api/order/find_drivers'
+
             headers = {'Content-Type': 'application/json; charset=utf-8',
                        'X-Requested-With': 'XMLHttpRequest'}
 
-            distance = 715934.21
-            mkad_distance = 23813.79
             cityfrom = form.cleaned_data.get('cityfrom', None)
             cityto = form.cleaned_data.get('cityto', None)
             weight = form.cleaned_data.get('weight', None)
@@ -22,8 +23,15 @@ def price(request):
             from_value = "Россия, " + str(cityfrom)
             to_value = "Россия, " + str(cityto)
 
+            geolocator = Yandex()
+            loc_from = geolocator.geocode(str(cityfrom))
+            loc_to = geolocator.geocode(str(cityto))
+            loc_from_coord = (loc_from.latitude, loc_from.longitude)
+            loc_to_coord = (loc_to.latitude, loc_to.longitude)
+            distance = vincenty(loc_from_coord, loc_to_coord).meters
+
             data_price = {"distance": distance, "weight": str(weight), "from_administrative_area": cityfrom,
-                          "to_administrative_area": cityto, "mkad_distance": mkad_distance,
+                          "to_administrative_area": cityto, "mkad_distance": 23813.79,
                           "is_refrigerator": False, "is_isotherm": False, "is_insurance": False,
                           "insurance_sum": 0, "porters_count": "0", "destination_points": {},
                           "destination_points_amount": 0,
